2cd70bf75d9c11429f774cfb618e3efacbd22ab1
Carlos Villanueva <ceva_19@hotmail.com>
2018-03-12 00:36:44 -0300

    JDBC Fundamentals - CRUD operations

--- JDBC-Fundamentals/src/ch02/connection/retrieving/SampleDBApp.java
+++ JDBC-Fundamentals/src/ch02/connection/retrieving/SampleDBApp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ch2.connection.retrieving;
+package ch02.connection.retrieving;
 
 import java.sql.*;
 /**
